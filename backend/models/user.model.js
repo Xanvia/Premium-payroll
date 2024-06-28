f2b0031b869e007fd3a2c0badf7b76450f255041
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-06-28 13:33:07 +0530

    feat : user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  nationalInsuranceNumber: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: false,
+    trim: true,
+    set: password => bcrypt.hashSync(password, 10) 
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

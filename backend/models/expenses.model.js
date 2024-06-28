73bd4c7f587439adc796a7a90ebec73cc1e31fca
Lakshitha Eranga <sanjueranga16@gmail.com>
2024-06-28 17:08:01 +0530

    feat: new shcemas

---
+++ backend/models/expenses.model.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const otherExpenseSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  car_miles: {
+    type: Number,
+    required: true,
+  },
+
+  file: {
+    type: String,
+    required: true,
+  },
+});
+
+const expensesSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  daysWorked: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  siteAddress: {
+    type: String,
+    required: true,
+  },
+  methodOfTransport: {
+    type: String,
+    required: true,
+    enum: ["CAR", "Bus", "Train"],
+  },
+  carMiles: {
+    type: Number,
+    required: true,
+  },
+  totalMileageClaim: {
+    type: Number,
+    required: true,
+  },
+  subsistence: {
+    type: Number,
+    required: true,
+  },
+  under8Hours: {
+    type: Number,
+    required: false,
+  },
+  subsistenceTotal: {
+    type: Number,
+    required: false,
+  },
+  otherExpenses: [otherExpenseSchema],
+});
+
+const Expenses = mongoose.model("Expenses", expensesSchema);
+
+module.exports = Expenses;
